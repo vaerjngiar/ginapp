5d8e0b968c45208dde98156c07504b44843bc0ba
vaerjngiar <vaerjngiar@gmail.com>
2018-03-07 09:31:37 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+var router *gin.Engine
+
+func main()  {
+	router = gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK,
+			"index.html",
+			gin.H{
+				"title": "Nome Page",
+			},
+		)
+	})
+
+	router.Run()
+}
